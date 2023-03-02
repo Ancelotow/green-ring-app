e95c7fb0b62714e83eabbaf757d670f982486871
Ancelotow <ancelotow@gmail.com>
2023-03-02 20:35:15 +0100

    refacto reward

--- lib/ui/forms/reward_form.dart
+++ lib/ui/forms/reward_form.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:green_ring/models/product.dart';
-
 import '../../models/notifications/submit_notification.dart';
 
 class RewardForm extends StatelessWidget {
