f6a5148f72abccdc572e775799848a027eb04dc7
Ancelotow <ancelotow@gmail.com>
2023-03-02 10:16:15 +0100

    merge with main

---
+++ lib/ui/admin_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: () => _scanBarcode(context),
+        tooltip: 'Scanner le produit',
+        child: const Icon(Icons.qr_code),
+      ),*/
+    );
+  }
+
+  void addGarbage(BuildContext context) {
+
+  }
+}
